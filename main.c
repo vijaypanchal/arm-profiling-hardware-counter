1b0d99cc2370fb4e3e14f1a71487c8ec4d57cbd5
vijay.kp <vijayp.work@gmail.com>
2023-08-13 07:18:01 +0100

    clean up

--- main.c
+++ main.c
@@ -16,23 +16,18 @@ static inline __uint64_t arch_timer_get_freq(void)
 }
 int main()
 {
-#if 0
-    printf("Before Sleep : %u\n",arch_timer_get_cntkctl());
-    sleep(2);
-    printf("After Sleep : %u\n",arch_timer_get_cntkctl());
-#else
+
     __uint64_t ts, te, freq;
-    //asm volatile("isb; mrs %0, cntvct_el0" : "=r"(ts));
+
     ts = arch_timer_get_cntkctl();
-    sleep(1);
+    sleep(2);
     te = arch_timer_get_cntkctl();
-    //asm volatile("isb; mrs %0, cntvct_el0" : "=r"(te));
+
     freq = arch_timer_get_freq();
-    //asm volatile("isb; mrs %0, cntfrq_el0" : "=r"(freq));
 
     printf("Aarch64 %ld cycles\n", (te - ts));
-
     printf(" Frequency = %ld\n", freq);
-#endif
+    printf("Time taken : %f \n",(float)(te-ts)/(float)freq);
+
     return 0;
 }
